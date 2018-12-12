9f018d7bb6bfedbd8ff75b1ff034a84b9e3234e4
Benjamin Zekavica <info@benjamin-zekavica.de>
2018-12-12 21:50:54 +0100

    Change

--- wp-source/tags/2.2/easy-svg.php
+++ wp-source/tags/2.2/easy-svg.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Easy SVG Support
+Plugin Name: Easy SVG Support 
 Plugin URI: https://wordpress.org/plugins/easy-svg/
 Description: Add SVG Support for WordPress.
-Version:     2.2
+Version:     2.0.3
 Author:      Benjamin Zekavica
 Author URI:  http://www.benjamin-zekavica.de
 Text Domain: easy-svg
@@ -24,34 +24,34 @@ You should have received a copy of the GNU General Public License
 along with Easy SVG. If not, see license.txt .
 
 
-Copyright by:
-(c) 2017 - 2018 by Benjamin Zekavica. All rights reserved.
+Copyright by: 
+(c) 2017 - 2018 by Benjamin Zekavica. All rights reserved. 
 
-Imprint:
+Imprint: 
 Benjamin Zekavica
-Oranienstrasse 12
-52066 Aachen
+Oranienstraße 12
+52066 Aachen 
 
 E-Mail: info@benjamin-zekavica.de
 Web: www.benjamin-zekavica.de
 
-I don't give support by Mail. Please write in the
-community forum for questions and problems.
+I don't give support by Mail. Please write in the 
+community forum for questions and problems.  
 
 
 */
 
 /* ========================================
-   Upload SVG Support
+   Upload SVG Support 
    ======================================== */
 
 add_filter( 'upload_mimes', 'esw_add_support' );
 
 
 function esw_add_support ( $svg_editing ){
-
+	
   $svg_editing['svg'] = 'image/svg+xml';
-
+  
   // Echo the svg file
   return $svg_editing;
 }
@@ -70,14 +70,14 @@ function esw_upload_check($checked, $file, $filename, $mimes){
   	 $type = $esw_upload_check['type'];
   	 $proper_filename = $filename;
 
-    /* ========================================
-        Upload checking filename
-       ======================================== */
+    /* ======================================== 
+        Upload checking filename 
+       ======================================== */ 	 
 
      if($type && 0 === strpos($type, 'image/') && $ext !== 'svg'){
       	$ext = $type = false;
      }
-
+     
      // Check the filename
      $checked = compact('ext','type','proper_filename');
 
@@ -91,40 +91,40 @@ add_filter('wp_check_filetype_and_ext', 'esw_upload_check', 10, 4);
 
 
 
-/* ========================================
-   Register activation hook.
+/* ======================================== 
+   Register activation hook. 
    ======================================== */
 
 register_activation_hook( __FILE__, 'esw_admin_notice_example_activation_hook' );
-
+ 
 
 /* ========================================
    Runs only when the plugin is activated.
    ======================================== */
 
 function esw_admin_notice_example_activation_hook() {
-
-    /* ========================================
-        Create transient data
+ 
+    /* ======================================== 
+        Create transient data 
        ======================================== */
 
     set_transient( 'esw-admin-notice-example', true, 5 );
 }
-
-
+ 
+ 
 /* Add admin notice */
 
 add_action( 'admin_notices', 'esw_admin_notice_example_notice' );
-
-
+ 
+ 
 /* ========================================
-   Admin Notice on Activation.
+   Admin Notice on Activation. 
    =======================================  */
 
 function esw_admin_notice_example_notice(){
-
+ 
     /* ==============================================
-       Check transient, if available display notice
+       Check transient, if available display notice 
        ============================================== */
 
     if( get_transient( 'esw-admin-notice-example' ) ){ ?>
@@ -132,9 +132,9 @@ function esw_admin_notice_example_notice(){
         <div class="updated notice is-dismissible">
             <p>
     				  <strong>
-    				  	<?php _e( 'Thank you for using this plugin!' , 'easy-svg' ) ; ?>
+    				  	<?php _e( 'Thank you for using this plugin!' , 'easy-svg' ) ; ?> 
     				  </strong><br />
-    				    <?php _e( 'Go now to the Media Libary and upload your SVG Files.' , 'easy-svg' ); ?>
+    				    <?php _e( 'Go now to the Media Libary and upload your SVG Files.' , 'easy-svg' ); ?> 
     				     <br /><br />
     				     <?php _e( 'Kind Regards', 'easy-svg' ); ?>
     				     <br />
@@ -154,15 +154,11 @@ function esw_admin_notice_example_notice(){
 
     }
 }
-
-
-  // Add Hook for Actived Plugin
-
-    add_action('esw_welcome_message_active_plugin', 'esw_admin_notice_example_notice');
-
-/* ========================================
-   Load Text Domain for languages files
-   =======================================  */
+   
+ 
+/* ======================================== 
+   Load Text Domain for languages files 
+   =======================================  */ 
 
 
    if ( ! function_exists( 'esw_multiligual_textdomain' ) ) :
@@ -173,50 +169,47 @@ function esw_admin_notice_example_notice(){
 
       }
 
-      // Add action for the translation file
+      // Add action for the translation file 
 
       add_action( 'plugins_loaded', 'esw_multiligual_textdomain' );
 
-      // Finishing the if loop
+      // Finishing the if loop 
 
     endif;
 
 
 
-  /* ========================================
-      Add WordPress Widget
-     =======================================  */
+  /* ======================================== 
+      Add WordPress Widget 
+     =======================================  */ 
 
 
     add_action('wp_dashboard_setup', 'esw_dashboard_widget');
-
+   
     function esw_dashboard_widget() {
       global $wp_meta_boxes;
-
+       
       wp_add_dashboard_widget('custom_help_widget', 'Easy SVG Support', 'esw_support_dashbord_widget_text');
     }
-
+       
 
     function esw_support_dashbord_widget_text() {
 
       _e( '<h2>Welcome to EASY SVG!</h2><br /> Thank you for your installtion of my custom plugin! Do you want to Upload SVG Files? Than go to the Media Libary and Upload it! <br /><br /> Best Regards <br /> <strong>Benjamin Zekavica<strong>', 'easy-svg' );
       echo "<br /><br />";
 
-      _e( 'Version Number', 'easy-svg' );
-      echo "&nbsp;2.2";
-
+      _e( 'Version Number', 'easy-svg' ); 
+      echo "&nbsp;2.0.3";
+   
     }
 
-    // Add Hook for Messages
 
-    add_action('esw_support_welcome_widget_text', 'esw_support_dashbord_widget_text');
-
-/* ========================================
-    Display SVG Files in Backend
-   =======================================  */
+/* ======================================== 
+    Display SVG Files in Backend 
+   =======================================  */ 
 
 
-   // Add JavaScipt to Backend
+   // Add JavaScipt to Backend 
 
 
     function esw_add_javascript_for_backend() {
@@ -227,7 +220,7 @@ function esw_admin_notice_example_notice(){
     add_action( 'admin_enqueue_scripts', 'esw_add_javascript_for_backend' );
 
 
-   // Echo
+   // Echo    
 
 
     add_action('wp_AJAX_svg_get_attachment_url', 'esw_display_svg_files_backend');
@@ -239,7 +232,6 @@ function esw_admin_notice_example_notice(){
       $url = '';
 
       $attachmentID = isset($_REQUEST['attachmentID']) ? $_REQUEST['attachmentID'] : '';
-
       if($attachmentID){
           $url = wp_get_attachment_url($attachmentID);
       }
