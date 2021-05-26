edbaef7c1473b8e7598e4902f9b6a6dce5d2e55a
Benjamin Zekavica <info@benjamin-zekavica.de>
2021-05-26 22:25:41 +0200

    Update easy-svg.php

--- easy-svg.php
+++ easy-svg.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy SVG Support
 Plugin URI:  https://wordpress.org/plugins/easy-svg/
 Description: Add SVG Support for WordPress.
-Version:     2.9.1
+Version:     3.0.0
 Author:      Benjamin Zekavica
 Author URI:  https://www.benjamin-zekavica.de
 Text Domain: easy-svg
